aff5351ae72ac227a28a2a10ae11474b6696f58d
Alex <alex.naga@hotmail.com>
2019-09-15 15:05:44 +0200

    Update content

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,65 +4,53 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function ($) {
+(function($) {
 
-	skel
-		.breakpoints({
-			xlarge: '(max-width: 1680px)',
-			large: '(max-width: 1280px)',
-			medium: '(max-width: 980px)',
-			small: '(max-width: 736px)',
-			xsmall: '(max-width: 480px)'
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#page-wrapper'),
+		$banner = $('#banner'),
+		$header = $('#header');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
 		});
 
-	$(function () {
-
-		var $window = $(window),
-			$body = $('body'),
-			$wrapper = $('#page-wrapper'),
-			$banner = $('#banner'),
-			$header = $('#header');
-
-		// Disable animations/transitions until the page has loaded.
-		$body.addClass('is-loading');
-
-		$window.on('load', function () {
-			window.setTimeout(function () {
-				$body.removeClass('is-loading');
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
 			}, 100);
 		});
 
-		// Mobile?
-		if (skel.vars.mobile)
+	// Mobile?
+		if (browser.mobile)
 			$body.addClass('is-mobile');
-		else
-			skel
-				.on('-medium !medium', function () {
-					$body.removeClass('is-mobile');
-				})
-				.on('+medium', function () {
-					$body.addClass('is-mobile');
-				});
+		else {
 
-		// Fix: Placeholder polyfill.
-		$('form').placeholder();
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
 
-		// Prioritize "important" elements on medium.
-		skel.on('+medium -medium', function () {
-			$.prioritize(
-				'.important\\28 medium\\29',
-				skel.breakpoint('medium').active
-			);
-		});
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
 
-		// Scrolly.
+		}
+
+	// Scrolly.
 		$('.scrolly')
 			.scrolly({
 				speed: 1500,
 				offset: $header.outerHeight()
 			});
 
-		// Menu.
+	// Menu.
 		$('#menu')
 			.append('<a href="#menu" class="close"></a>')
 			.appendTo($body)
@@ -77,24 +65,19 @@
 				visibleClass: 'is-menu-visible'
 			});
 
-		// Header.
-		if (skel.vars.IEVersion < 9)
-			$header.removeClass('alt');
-
+	// Header.
 		if ($banner.length > 0
-			&& $header.hasClass('alt')) {
+		&&	$header.hasClass('alt')) {
 
-			$window.on('resize', function () { $window.trigger('scroll'); });
+			$window.on('resize', function() { $window.trigger('scroll'); });
 
 			$banner.scrollex({
-				bottom: $header.outerHeight() + 1,
-				terminate: function () { $header.removeClass('alt'); },
-				enter: function () { $header.addClass('alt'); },
-				leave: function () { $header.removeClass('alt'); }
+				bottom:		$header.outerHeight() + 1,
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
 			});
 
 		}
-
-	});
 
 })(jQuery);
